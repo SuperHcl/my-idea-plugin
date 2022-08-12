fa19fee5d917a02dc1b1a09ce3095f3f9fe092a9
GitHub Action <action@github.com>
2022-08-12 09:14:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/superhcl/myideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/superhcl/myideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.superhcl.myideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.superhcl.myideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/superhcl/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/superhcl/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.superhcl.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.superhcl.myideaplugin.MyBundle
 
 class MyApplicationService {
 
